b473994fcae5beae2ee62344e2f9eb87d4cf3302
FrancescoSica16 <francesco.sica16@gmail.com>
2021-09-15 16:28:30 +0200

    indexImages in html

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var app = new Vue({
     el: '#app',
 
     data: {
+
+        // myclass: "classe promva",
+
       images : [
           {
             source: "https://assets.change.org/photos/6/xg/aq/HrXGaqjJbVlPYcq-800x450-noPad.jpg?1525088692",
@@ -16,10 +19,21 @@ var app = new Vue({
             source: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/TG5_Prima_Pagina_-_Logo_2018.svg/1200px-TG5_Prima_Pagina_-_Logo_2018.svg.png",
             alt:"TG 5"
           },
-      ]
+      ],
+      indexImages : 0,  // errore se lo metto fuori da images
     },
 
-    methods: {
+    
 
-    }
+    methods: {
+        previousImages : function(){   // posso scrivere le funzione solo cosi ?
+            this.indexImages--;   //perche metto this ?
+            console.log(this.indexImages) 
+        },
+        nextImages : function(){   
+            this.indexImages++;
+            console.log(this.indexImages)   
+        },
+       
+    },
   })
