05be1dedf3f58ed97e01d69b4dcd5e394f9e15d0
FrancescoSica16 <francesco.sica16@gmail.com>
2021-09-15 16:00:36 +0200

    inserimento img

---
+++ script.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+
+    el: '#app',
+
+    data: {
+      images : [
+          {
+            source: "https://assets.change.org/photos/6/xg/aq/HrXGaqjJbVlPYcq-800x450-noPad.jpg?1525088692",
+            alt:"L'ora esatta"
+          },
+          {
+            source: "https://www.vivodibenessere.it/wp-content/uploads/2010/12/giorno-e-notte.jpg",
+            alt:"Giorno e notte"
+          },
+          {
+            source: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/TG5_Prima_Pagina_-_Logo_2018.svg/1200px-TG5_Prima_Pagina_-_Logo_2018.svg.png",
+            alt:"TG 5"
+          },
+      ]
+    },
+
+    methods: {
+
+    }
+  })
